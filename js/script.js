17a36c5a9e0335c10d3613b2bcbd2bd6c3069fbc
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-23 15:25:49 +0100

    test with bug

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*
+Consegna:
+Scrivi un programma che stampi in console i numeri da 1 a 100:
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e
+per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+
+
+
+for (let i=1 ; i<=100 ; i++){
+    
+    if ( i % 3 ){
+        console.log('fizz')
+    } else if (i % 5 ){
+        console.log('buzz')
+    } else {
+        console.log (i)
+    }
+   
+  }   
+  
+  

78d4931fc021e772ed62d4d532e62cc3543fd5cc
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-23 17:41:06 +0100

    Fizzbzz in js completed

--- js/script.js
+++ js/script.js
@@ -8,17 +8,19 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
 
-for (let i = 0 ; i<=100 ; i++){
+for (let i = 1 ; i<=100 ; i++){
       
     
-    if ( i % 3 == 0 ){
+    if ( i % 3 == 0 && i % 5 == 0){
+        console.log('FizzBuzz')
+    } else if ( i % 3 == 0 ){
         console.log('Fizz')
     } else if (i % 5 == 0){
         console.log('Buzz')
-    } else {
+    } else  {
         console.log (i)
-    }
    
   }   
-  
+
+}
   
